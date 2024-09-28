c9564bfe97e51e2c2fd8f1d31297b26b48792b08
Andrei Dmitriev <deseq97@mail.ru>
2024-09-28 18:43:41 +0300

    test

--- download_files.py
+++ download_files.py
@@ -1,4 +1,5 @@
-import urllib
+import requests
+
 import os
 
 data_train = []
@@ -11,5 +12,10 @@ with open('train.csv', 'r') as f:
 
 os.makedirs('dataset', exist_ok=True)
 for i in data_train[:1000]:
-    testfile = urllib.URLopener()
-    testfile.retrieve(f'{i[1]}', f'dataset/{i[0]}.mp4')
+    url = i[1]
+    response = requests.get(url)
+    file_path = f'dataset/{i[0]}.mp4'
+    if response.status_code == 200:
+        with open(file_path, 'wb') as file:
+            file.write(response.content)
+    # testfile.retrieve(f'{i[1]}', f'dataset/{i[0]}.mp4')
